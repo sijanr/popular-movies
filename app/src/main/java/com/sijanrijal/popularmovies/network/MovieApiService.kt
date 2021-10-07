b16a32e097561e72882d3c93180a6e3d2d699aa7
sijanr <sijan1.rijal@gmail.com>
2021-10-06 18:18:55 -0700

    Setup network api

---
+++ app/src/main/java/com/sijanrijal/popularmovies/network/MovieApiService.kt
@@ -0,0 +1,11 @@
+package com.sijanrijal.popularmovies.network
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApiService {
+
+    @GET("movie/now_playing")
+    suspend fun getNowPlayingMovies(@Query("api_key") apiKey: String): Response<MovieResponse>
+}
